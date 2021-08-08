a1bc70eaad666871b78881e29555d17f3d1d0451
eunbean <ds5bean23@gmail.com>
2021-08-08 22:31:46 +0900

    reinstalljslint

--- styling-react/src/App.js
+++ styling-react/src/App.js
@@ -1,16 +1,16 @@
 // src\App.js
 
 import React, { Component } from 'react';
-import SassComponent from './SassComponent';
+import StyledComponent from './StyledComponent';
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        <SassComponent />
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<StyledComponent />
+			</div>
+		);
+	}
 }
 
 export default App;
