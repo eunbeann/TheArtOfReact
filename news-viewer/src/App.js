be61841598f8c3c69373ef114fade65f3c9c880a
eunbean <ds5bean23@gmail.com>
2021-08-19 23:57:51 +0900

    0819bfUIsetting

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,13 +1,20 @@
+//src/App.js
+
 import React, {useState} from "react";
 import axios from 'axios';
 
 const App = () => {
   const [data, setData] = useState(null);
-  const onClick = () => {
-    axios.get("https://jsonplaceholder.typicode.com/todos/1").then(Response => {
-      setData(Response.data);
-    });
-  }
+  const onClick = async () => {
+    try {
+      const response = await axios.get(
+        "https://newsapi.org/v2/top-headlines?country=kr&apiKey=d50b9493091443a48657199e0a0b4c22",
+      );
+      setData(response.data);
+    } catch(e) {
+      console.log(e);
+    }
+  };
   return (
     <div>
       <button onClick={onClick}>불러오기</button>
