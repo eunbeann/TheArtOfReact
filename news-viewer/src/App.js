b45963e46681d5ff71da279365ea6e339ad203ef
eunbean <ds5bean23@gmail.com>
2021-08-22 00:40:14 +0900

    p378

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,25 +1,16 @@
 //src/App.js
 
-import React, {useState} from "react";
-import axios from 'axios';
+import React from "react";
+import Categories from "./components/Categories";
+import NewsList from "./components/NewsList";
+
 
 const App = () => {
-  const [data, setData] = useState(null);
-  const onClick = async () => {
-    try {
-      const response = await axios.get(
-        "https://newsapi.org/v2/top-headlines?country=kr&apiKey=d50b9493091443a48657199e0a0b4c22",
-      );
-      setData(response.data);
-    } catch(e) {
-      console.log(e);
-    }
-  };
   return (
-    <div>
-      <button onClick={onClick}>불러오기</button>
-    {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true} />}
-    </div>
+  <>
+    <Categories />
+    <NewsList />
+    </>
   );
 };
 
