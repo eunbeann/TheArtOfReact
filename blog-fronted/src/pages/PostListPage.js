0dc666b9a0687896a82d2aa63723279aca713bc9
eunbean <ds5bean23@gmail.com>
2021-09-01 12:33:07 +0900

    873

--- blog-fronted/src/pages/PostListPage.js
+++ blog-fronted/src/pages/PostListPage.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Header from '../components/common/Header';
+import PostList from '../components/posts/PostList';
+import Pagination from '../components/posts/Pagination';
 
 const PostListPage = () => {
     return (
         <>
         <Header />
-        <div>안녕하세요.</div>
+        <PostList />
+        <Pagination />
         </>
     );
 };
