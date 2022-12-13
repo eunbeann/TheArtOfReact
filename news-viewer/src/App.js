0a0758612d2083c1f91362a87cee997833c993af
eunbeann <ds5bean23@gmail.com>
2022-12-13 17:54:30 +0900

    [feat] Create Components + Mock Image

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,29 +1,8 @@
-import { useState } from "react";
-import axios from "axios";
-
-import React from 'react'
-
+import NewsList from "./components/NewsList";
 const App = () => {
-  const WeatherAPIKEY = process.env.REACT_APP_WEATHER_API_KEY;
-  // console.log(WeatherAPIKEY);
-  const [data, setData] = useState(null);
-  const onClick = async () => {
-    try {
-      const response = await axios.get(
-        `https://newsapi.org/v2/top-headlines?country=kr&apiKey=${WeatherAPIKEY}`,
-      );
-      setData(response.data);
-    } catch (e) {
-      console.log(e);
-    };
-  }
   return (
-    <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true} />}
-    </div>
-  );
-};
+    <NewsList />
+  )
+}
+
 export default App
