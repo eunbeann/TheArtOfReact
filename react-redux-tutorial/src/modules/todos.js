ea56a287085ad9faed6f4dc7b0b4552aedcd33ea
eunbeann <ds5bean23@gmail.com>
2022-12-29 13:46:55 +0900

    460p

--- react-redux-tutorial/src/modules/todos.js
+++ react-redux-tutorial/src/modules/todos.js
@@ -1,39 +1,29 @@
 //modules/todos.js
 
+import { createAction } from "redux-actions";
+
 const CHANGE_INPUT = 'todos/CHANGE_INPUT'; // 인풋 값 변경
 const INSERT = 'todos/INSERT'; //새로운 todo를 등록함
 const TOGGLE = 'todos/TOGGLE'; //todo를 체크/ 체크 해제함
 const REMOVE = 'todos/REMOVE'; // todo를 제거함
 
-export const changeInput = input => ({
-    type: CHANGE_INPUT,
-    input
-});
+export const changeInput = createAction(CHANGE_INPUT,
+    input => input
+);
 
 let id = 3; //insert가 호출될 때마다 1씩 더해짐
 // 다음 절에서 초기 상태 작성할 때 todo 객체 두 개를 사전에 미리 넣어둘 것이므로 그다음에 새로 추가될 항목의 id가 3임
 
-export const insert = text => ({
-    // 액션 객체를 만들 때 파라미터 외에 사전에 이미 선언되어 있는 id라는 값에도 의존 함.
-    type: INSERT,
-    todo: {
+export const insert = createAction(INSERT, text => ({
         id: id++,
         // 호출될 때마다 id 값에 1씩 더해 줌. 
         // 이 id 값은 todo 객체가 들고 있게 될 고윳값
         text,
         done: false
-    }
-});
+    }));
 
-export const toggle = id => ({
-    type: TOGGLE,
-    id
-});
-
-export const remove = id => ({
-    type: REMOVE,
-    id
-})
+export const toggle = createAction(TOGGLE, id => id);
+export const remove = createAction(REMOVE, id => id);
 
 const initialState = {
     input: '',
