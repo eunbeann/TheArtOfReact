f12117a02657683f0b61af76482151c4619d84b9
eunbeann <ds5bean23@gmail.com>
2023-01-19 15:01:55 +0900

    feat: Done of Redux Project

--- learn-redux-middleware/src/index.js
+++ learn-redux-middleware/src/index.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
-import rootReducer from './modules';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import rootReducer from './modules';
+import { createStore } from 'redux';
 
+const store = createStore(rootReducer);
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-const store = createStore = createStore(rootReducer);
 root.render(
-  <Provider store = {store}>
+  <React.StrictMode>
+    <Provider store={store}>
     <App />
-  </Provider>
+    </Provider>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
