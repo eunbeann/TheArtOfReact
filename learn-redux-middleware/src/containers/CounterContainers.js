87d0c6f32391d2a8d7562a6ce265d8e97c5ab60e
eunbeann <ds5bean23@gmail.com>
2023-01-25 17:21:28 +0900

    thunk 생성 함수 만들기

--- learn-redux-middleware/src/containers/CounterContainers.js
+++ learn-redux-middleware/src/containers/CounterContainers.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { connect } from 'react-redux';
-import { increase, decrease } from '../modules/counter'
+import { increaseAsync, decreaseAsync } from '../modules/counter'
 import Counter from '../compoenets/Counter';
 
-const CounterContainers = ({ number, increase, decrease }) => {
+const CounterContainers = ({ number, increaseAsync, decreaseAsync }) => {
     return (
-        <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+        <Counter number={number} onIncrease={increaseAsync} onDecrease={decreaseAsync} />
     );
 };
 
@@ -14,7 +14,7 @@ export default connect(
         number: state.counter
     }),
     {
-        increase,
-        decrease
+        increaseAsync,
+        decreaseAsync
     }
 )(CounterContainers);
--- learn-redux-middleware/src/index.js
+++ learn-redux-middleware/src/index.js
@@ -9,9 +9,10 @@ import { Provider } from 'react-redux';
 import rootReducer from './modules';
 import { applyMiddleware, createStore } from 'redux';
 import { createLogger } from 'redux-logger';
+import ReduxThunk from 'redux-thunk';
 
 const logger = createLogger();
-const store = createStore(rootReducer, applyMiddleware(logger));
+const store = createStore(rootReducer, applyMiddleware(logger, ReduxThunk));
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
